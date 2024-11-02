58b77e1a1ff278bc2aeb9a38438b12f0d5439925
James Koh <huisoonkoh@gmail.com>
2024-11-02 12:41:45 +0800

    Chapter 01 Hello World

---
+++ c01-hello-world/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    /*
+     * println! is a macro
+     */
+    println!("Hello, world!");
+    /*
+     * Semicolon?
+     * https://stackoverflow.com/questions/26665471/are-semicolons-optional-in-rust
+     */
+    println!("I'm a Rustacean!")
+}
