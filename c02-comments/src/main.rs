8c7d681e9198fa72fb8c9dd718961c91a8e22b8e
James Koh <huisoonkoh@gmail.com>
2024-11-02 14:13:39 +0800

    Chapter 02 Comments

---
+++ c02-comments/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    /*
+     * There are different types of comments
+     *
+     * Regular comments
+     * //       single line comment
+     * /* */    multi line comment
+     *
+     * Doc comments, which can be parsed into HTML library documentation
+     * ///      generate library docs for the following item
+     * //!      generate library docs for the enclosing item
+     */
+}
